3d1a3aaefd1a5b8f55d91a99076fd8542f1cff05
Satyawan Panchal <panchal.satyawan@gmail.com>
2024-11-05 07:58:00 +0530

    with backend url in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'http://82.112.231.58:5000/api' });
 
 export const fetchStudents = () => API.get('/students');
 export const createStudent = (student) => API.post('/students', student);
