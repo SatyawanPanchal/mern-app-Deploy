2e8bfda66f18e07b4830aee331aaa95a816dc238
Satyawan Panchal <panchal.satyawan@gmail.com>
2024-11-05 13:09:42 +0530

    commit 3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://api.roshnieducation.com/api' });
+const API = axios.create({ baseURL: "https://api.roshnieducation.com" });
 
-export const fetchStudents = () => API.get('/students');
-export const createStudent = (student) => API.post('/students', student);
-export const updateStudent = (id, updatedStudent) => API.put(`/students/${id}`, updatedStudent);
+export const fetchStudents = () => API.get("/students");
+export const createStudent = (student) => API.post("/students", student);
+export const updateStudent = (id, updatedStudent) =>
+  API.put(`/students/${id}`, updatedStudent);
 export const deleteStudent = (id) => API.delete(`/students/${id}`);
