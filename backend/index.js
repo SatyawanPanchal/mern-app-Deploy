54838ff1a3f6b9b35e03f72e742f2b08ee138388
Satyawan Panchal <panchal.satyawan@gmail.com>
2024-11-05 07:51:06 +0530

    commit with msg on server

--- backend/index.js
+++ backend/index.js
@@ -21,4 +21,9 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/students', studentRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send("i am working")
+ 
+    
+})
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
